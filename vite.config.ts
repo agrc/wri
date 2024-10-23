307cecc92d5e77df01f4a57b3f65b21f59bea324
steveoh <sgourley@utah.gov>
2024-10-23 10:04:49 -0600

    build: set the asset dir and main input html (#24)

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import loadVersion from 'vite-plugin-package-version';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), loadVersion()],
+  base: './',
+  build: {
+    assetsDir: './wri/js/ugrc',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'map.html'),
+        dev: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
 });

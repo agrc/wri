6c1cb2c487a76914583fbcf32001a82e4521507b
steveoh <sgourley@utah.gov>
2024-11-12 15:38:21 -0700

    chore: fix dark mode text color

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,7 @@ export default function App() {
       <section className="relative flex min-h-0 flex-1 gap-2">
         <Drawer main state={sideBarState} {...sideBarTriggerProps}>
           <div className="mx-2 mb-2 grid grid-cols-1 gap-2">
-            <h2 className="text-xl font-bold">Map controls</h2>
+            <h2 className="text-xl font-bold dark:text-zinc-200">Map controls</h2>
             <div className="flex flex-col gap-4 rounded border border-zinc-200 p-3 dark:border-zinc-700">
               <ErrorBoundary FallbackComponent={ErrorFallback}>
                 <h5 className="dark:text-zinc-200">Search tool</h5>

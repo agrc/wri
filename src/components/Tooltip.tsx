2c5a717a81bb54670b25135fae77ffc2a7beedcc
steveoh <sgourley@utah.gov>
2024-12-12 21:04:54 -0700

    refactor: allow tooltip to be disabled
    add pointer events none so it doesn't block the map tools

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -9,19 +9,38 @@ type GraphicAttributes = {
   StatusDescription?: string;
 };
 
-export const Tooltip = ({ view, layers }: { view: __esri.MapView; layers: __esri.FeatureLayer[] }) => {
+export const Tooltip = ({
+  view,
+  layers,
+  enabled,
+}: {
+  view: __esri.MapView;
+  layers: __esri.FeatureLayer[];
+  enabled: boolean;
+}) => {
   const tooltipNode = useRef<HTMLDivElement | null>(null);
   const currentVisibleTooltipId = useRef<number | null>(null);
-
+  const pointerHandler = useRef<__esri.Handle | null>(null);
   const [hoverProject, setHoverProject] = useState<GraphicAttributes | null>(null);
   const isReady = useMapReady(view);
 
   useEffect(() => {
-    if (!tooltipNode.current || !isReady) {
+    if (!enabled || !tooltipNode.current || !isReady) {
+      if (pointerHandler.current) {
+        pointerHandler.current.remove();
+        pointerHandler.current = null;
+      }
+
+      if (tooltipNode.current) {
+        tooltipNode.current.classList.add('duration-1000');
+        tooltipNode.current.classList.replace('opacity-90', 'opacity-0');
+        currentVisibleTooltipId.current = null;
+      }
+
       return;
     }
 
-    view.on('pointer-move', (event) => {
+    pointerHandler.current = view.on('pointer-move', (event) => {
       const opts = {
         include: layers,
       };
@@ -59,10 +78,20 @@ export const Tooltip = ({ view, layers }: { view: __esri.MapView; layers: __esri
         }
       });
     });
-  }, [isReady, layers, view]);
+
+    return () => {
+      if (pointerHandler.current) {
+        pointerHandler.current.remove();
+        pointerHandler.current = null;
+      }
+    };
+  }, [enabled, isReady, layers, view]);
 
   return (
-    <div ref={tooltipNode} className="absolute z-10 h-20 w-56 rounded border bg-white opacity-0 transition-opacity">
+    <div
+      ref={tooltipNode}
+      className="pointer-events-none absolute z-10 h-20 w-56 rounded border bg-white opacity-0 transition-opacity"
+    >
       <div className="p-2 text-sm">
         <h3 className="truncate text-sm font-bold">{hoverProject?.Title}</h3>
         <p>Project: {hoverProject?.Project_ID}</p>

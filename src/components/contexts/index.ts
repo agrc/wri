c9186076e8062b62ec97bcec0b4a2d1b73a649a3
steveoh <sgourley@utah.gov>
2025-01-28 17:41:36 -0700

    chore: remove unused code

--- src/components/contexts/index.ts
+++ src/components/contexts/index.ts
@@ -1,7 +1,5 @@
 export * from './FilterContext';
 export * from './FilterProvider';
-export * from './LayerViewContext';
-export * from './LayerViewProvider';
 export * from './MapProvider';
 export * from './ProjectContext';
 export * from './ProjectProvider';

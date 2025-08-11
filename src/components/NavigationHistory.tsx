fc1ded2d065c2c8c62f310d008328b40ed42df46
steveoh <sgourley@utah.gov>
2025-08-11 17:24:01 -0600

    style: add dark mode to navigation

--- src/components/NavigationHistory.tsx
+++ src/components/NavigationHistory.tsx
@@ -1,7 +1,7 @@
 import { watch } from '@arcgis/core/core/reactiveUtils';
 import { Button } from '@ugrc/utah-design-system';
 import { useViewUiPosition } from '@ugrc/utilities/hooks';
-import clsx from 'clsx';
+import { clsx } from 'clsx';
 import type { WritableDraft } from 'immer';
 import { Redo2Icon, Undo2Icon } from 'lucide-react';
 import { useEffect, useRef } from 'react';
@@ -90,9 +90,9 @@ export const NavigationHistory = ({
   const backwardIsDisabled = state.index === 0;
   const forwardIsDisabled = state.index >= state.history.length - 1;
   const iconClasses =
-    'dark:text-zinc-500 size-5 stroke-[1.5] transition-colors duration-150 ease-in-out will-change-transform group-enabled/button:[#6e6e6e] group-enabled/button:group-hover/button:text-[#151515] group-disabled/button:[#cfcfcf] group-disabled/button:opacity-50';
+    'dark:text-[#9e9e9e] size-5 stroke-[1.5] transition-colors duration-150 ease-in-out will-change-transform group-enabled/button:[#6e6e6e] group-enabled/button:group-hover/button:text-[#151515] dark:group-enabled/button:group-hover/button:text-white group-disabled/button:[#cfcfcf] group-disabled/button:opacity-50';
   const buttonContainerClasses =
-    'group/icon flex size-[32px] items-center justify-center bg-white shadow-[0_1px_2px_#0000004d]';
+    'group/icon flex size-[32px] items-center justify-center bg-white dark:bg-zinc-800 dark:ring-white/10 shadow-[0_1px_2px_#0000004d]';
   const buttonClasses =
     'group/button size-full stroke-[4] p-0 transition-colors duration-150 ease-in-out will-change-transform focus:min-h-0 focus:outline-offset-[-2px] group/icon-hover:bg-[#f3f3f3]';
 
